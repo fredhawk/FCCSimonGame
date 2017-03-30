b610c213136ee878704cdd88e438cc70fd87c3ab
eyece <fredrichawk@gmail.com>
2017-03-30 06:33:11 +0200

    Added image processing to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const autoprefixer = require('autoprefixer');
 const path = require('path');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
   devServer: {
     contentBase: path.resolve(__dirname, './src'),
     port: 9000,
-    stats: "errors-only",
-    open: true
+    stats: 'errors-only',
+    open: true,
   },
   module: {
     rules: [
@@ -21,38 +21,41 @@ module.exports = {
         test: /\.js$/,
         exclude: [/node_modules/],
         use: [
-            'babel-loader',
-            'eslint-loader',
-          ]
+          'babel-loader',
+          'eslint-loader',
+        ],
       }, {
-        test: /\.scss$/, 
+        test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [{
             loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1}
-          }, { 
+            options: { sourceMap: true, importLoaders: 1 },
+          }, {
             loader: 'postcss-loader',
             options: {
               sourceMap: true,
               plugins: () => [
-                require('autoprefixer')(),
+                autoprefixer(),
               ],
-            }
+            },
           },
           {
-            loader: 'sass-loader'
+            loader: 'sass-loader',
           }],
-          publicPath: '/dist'
-        })
-      }
-    ]
+          publicPath: '/dist',
+        }),
+      }, {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader?name=[name].[ext]&outputPath=images/',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Simon Says',
       minify: {
-        collapseWhitepsace: false // Set to true to minify html
+        collapseWhitepsace: false, // Set to true to minify html
       },
       hash: true, // Set to true if we want hashed bundles.
       template: './src/index.html', // Load a custom template (ejs by default)
@@ -60,7 +63,7 @@ module.exports = {
     new ExtractTextPlugin({
       filename: 'style.css',
       disable: false,
-      allChunks: true
-    })
-    ]
-}
+      allChunks: true,
+    }),
+  ],
+};
