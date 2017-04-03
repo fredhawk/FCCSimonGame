9977e723d889f4b53e33d57491ff59557d3b2580
eyece <fredrichawk@gmail.com>
2017-04-03 06:32:40 +0200

    added sounds and a couple of future methods

--- src/app.js
+++ src/app.js
@@ -10,6 +10,18 @@ const game = {
   currentSequence: [],
   isStrict: false,
   roundCount: 0,
+  gameSounds: {
+    yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+    green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+    red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
+  },
+  startGame() {
+    // start game
+  },
+  resetGame() {
+
+  },
   makeTurn() {
     this.current.push(randomizer(this.colors));
   },
